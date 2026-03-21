e50ac1c3180697d01e4d754192e1802ef475312f
ifeanyiD <engineerdavid004@gmail.com>
2026-03-21 22:35:43 +0100

    correction on the url in the origin

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ const io = new Server(server, {
   cors: {
     origin: [
       "http://localhost:5173",
-      "https://chizzyevent.netlify.app/"
+      "https://chizzyevent.netlify.app"
     ],
     credentials: true
   }
